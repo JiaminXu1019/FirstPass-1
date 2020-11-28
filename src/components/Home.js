744985f8fa03e80824130eff79cfec06d3c52aa9
ethan <ethanwong@g.ucla.edu>
2020-11-27 20:21:55 -0800

    Created parent class for standingRadioButton and classDropdown

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,6 +22,7 @@ class StandingRadioButton extends React.Component {
   onValueChange(event) {
     this.setState({
       selectedOption: event.target.value
+      //selectedOption: this.props.parentState.selectedOption
     });
   }
 
@@ -39,8 +40,8 @@ class StandingRadioButton extends React.Component {
             <input
               type="radio"
               value="Freshman"
-              checked={this.state.selectedOption === "Freshman"}
-              onChange={this.onValueChange}
+              checked={this.props.parentState === "Freshman"}
+              onChange={this.props.onValueChange}
             />
             Freshman
           </label>
@@ -50,8 +51,8 @@ class StandingRadioButton extends React.Component {
             <input
               type="radio"
               value="Sophomore"
-              checked={this.state.selectedOption === "Sophomore"}
-              onChange={this.onValueChange}
+              checked={this.props.parentState === "Sophomore"}
+              onChange={this.props.onValueChange}
             />
             Sophomore
           </label>
@@ -61,8 +62,8 @@ class StandingRadioButton extends React.Component {
             <input
               type="radio"
               value="Junior"
-              checked={this.state.selectedOption === "Junior"}
-              onChange={this.onValueChange}
+              checked={this.props.parentState === "Junior"}
+              onChange={this.props.onValueChange}
             />
             Junior
           </label>
@@ -72,15 +73,16 @@ class StandingRadioButton extends React.Component {
             <input
               type="radio"
               value="Senior"
-              checked={this.state.selectedOption === "Senior"}
-              onChange={this.onValueChange}
+              checked={this.props.parentState === "Senior"}
+              onChange={this.props.onValueChange}
             />
             Senior
           </label>
         </div>
+
         {/* <div className="selected">
-          Selected option is : {this.state.selectedOption}
-        </div> */}
+          Selected option is : {this.props.parentState.selectedOption}
+        </div>  */}
         <button className="btn" type="submit">
           Submit
         </button>
@@ -90,31 +92,31 @@ class StandingRadioButton extends React.Component {
   }
 }
 
-class ClassDropdown extends React.Component {
 
+class ClassDropdown extends React.Component {
   constructor(props) {
       super(props);
-
       this.state = {
-          class1: "",
+          classes: "",
       };
       this.handleChange = this.handleChange.bind(this);
+      this.selectChange = this.selectChange.bind(this);
   }
 
   handleChange = e => {
-      this.setState({
+      this.setState({  //
           [e.target.name]: e.target.value
       });
   };
 
   selectChange = (val, name) => {
-      this.setState({
+      this.setState({  
           [name]: val
       });
   }
 
   render() {
-      console.log(this.state);
+      //var selectChange = this.props.selectChange;
       return(
           <div className = "container">
               <form className = "inside"> 
@@ -124,28 +126,64 @@ class ClassDropdown extends React.Component {
                       options={class_names}
                       search
                       value={this.state.class1}
-                      onChange={val => this.selectChange(val, "class1")}
+                      //onChange={val1 => this.selectChange(val1, "classes") }
+                      onChange={val => this.props.selectChange(val, "classes")}   ////////
                   />
               </form> 
           </div>
       );
-  }
+    }
 }
 
-class Home extends React.Component {
 
+class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       classes: "",
       selectedOption: ""
     };
+    this.onValueChange = this.onValueChange.bind(this);
+    this.formSubmit = this.formSubmit.bind(this);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.selectChange = this.selectChange.bind(this);
   }
+  //standingRadioButton Functions
+  onValueChange(event) {
+    this.setState({
+      selectedOption: event.target.value
+    });
+  }
+  formSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.selectedOption)
+  }
+  //classDropdown Functions
+  handleChange = e => {
+    this.setState({
+        [e.target.name]: e.target.value
+    });
+  };
+  selectChange = (val, name) => {
+    this.setState({
+        [name]: val
+    });
+  }  
 
   render() {
     return (
       <div className="radio-buttons">
-        <StandingRadioButton /><ClassDropdown />
+        <StandingRadioButton 
+          onValueChange = {this.onValueChange}
+          formSubmit = {this.formSubmit}
+          parentState = {this.state.selectedOption}
+        />
+        <ClassDropdown 
+          handleChange = {this.handleChange}
+          selectChange = {this.selectChange}
+          classes = {this.state.classes}
+        />
       </div>
     );
   } 
